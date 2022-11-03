27824e4a6b83722d977ed15c421cedb7cab196a6
AnastasiyaPlotnikova <world771@yandex.ru>
2022-11-03 21:00:26 +0500

    new zadacha2 added

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Максимальное число: " + numberA);
+    Console.WriteLine("Минимальное число: " + numberB);
+}
+else
+{
+    Console.WriteLine("Максимальное число: " + numberB);
+    Console.WriteLine("Мминимальное число: " + numberA);
+}
