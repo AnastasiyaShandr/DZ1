5bd2eb9ab73543c491facca646a708fb52110c51
AnastasiyaPlotnikova <world771@yandex.ru>
2022-11-03 21:31:43 +0500

    new zadacha8 added

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
+Console.WriteLine();
